c0c5431837f9ae187bc3ebd60762f48820b10bd1
Alejandro <alejandro@animasoluciones.es>
2019-08-26 10:44:14 +0200

    mobile flag and menu animate

--- js/recaudacion_flag.js
+++ js/recaudacion_flag.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     $(".wrapper_flag_recaudacion").click(function(){
-        $(".wrapper_img_recaudacion").removeClass("d-none");
-        $(".wrapper_flag_recaudacion").addClass("d-none");
+        $(".wrapper_flag_recaudacion").addClass("animated fadeOutRight");
+        $(".wrapper_img_recaudacion").removeClass("d-none fadeOutRight");
+        $(".wrapper_img_recaudacion").addClass("animated fadeInRight");
     });
 
     $(".wrapper_img_recaudacion").click(function(){
-        $(".wrapper_flag_recaudacion").removeClass("d-none");
-        $(".wrapper_img_recaudacion").addClass("d-none");
+        $(".wrapper_flag_recaudacion").removeClass("fadeOutRight");
+        $(".wrapper_img_recaudacion").addClass("fadeOutRight");
+        $(".wrapper_flag_recaudacion").addClass("fadeInRight");
     });
   });
--- js/toggle_menu.js
+++ js/toggle_menu.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
   $(".btn_openNav").click(function(){
-    $(".sidenav").css("width", "210px");
+    openNav();
   });
   
   $(".btn_closeNav").click(function(){
-    $(".sidenav").css("width", "0px");
+    closeNav();
   });
+
+  $("#myOverlay").click(function(){
+    closeNav();
+  });
+
+  function openNav() {
+    $(".sidenav").removeClass("fadeOutLeft");
+    $(".sidenav").css("width", "210px");
+    $(".sidenav").addClass("animated fadeInLeft");
+    $("#myOverlay").css("display", "block");
+  }
+  
+  function closeNav() {
+    $(".sidenav").removeClass("fadeInLeft");
+    $(".sidenav").addClass("animated fadeOutLeft");
+    $("#myOverlay").css("display", "none");
+  }
+
 });
